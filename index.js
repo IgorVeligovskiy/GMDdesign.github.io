05793804b8f34ee12c7c7e7b88b4fe925d2d0e6f
Sergey <66194887+GMDdesign@users.noreply.github.com>
2021-03-30 15:22:20 +0700

    132
    new commit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+function fasterPreview(uploader) {
+    if (uploader.files && uploader.files[0]) {
+        $('#profileImage').attr('src',
+            window.URL.createObjectURL(uploader.files[0]));
+    }
 }
+
+$("#imageUpload").change(function() {
+    fasterPreview(this);
+});
+
+input.onmouseover(function(event) {
+    let target = event.target;
+    document.getElementById("search-panel").style.height = '200px';
+    text.value += "mouseover " + target.tagName + "\n";
+    text.scrollTop = text.scrollHeight;
+});
+
+input.onmouseout(function(event) {
+    let target = event.target;
+    document.getElementById("search-panel").style.height = '80px';
+    text.value += "mouseout " + target.tagName + "\n";
+    text.scrollTop = text.scrollHeight;
+});
